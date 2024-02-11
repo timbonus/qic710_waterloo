db861fc3ab21f91907d9853f580f81170245020f
timbonus <timbonus@gmail.com>
2024-02-11 20:35:07 +1100

    debugging and unit testing of to_euler

--- qubit.py
+++ qubit.py
@@ -7,23 +7,26 @@ def qubit_norm(state: np.array):
 
 class QubitCartesian:
 
-    def __init__(self, bra: np.complex_, ket: np.complex_):
+    def __init__(self, zero: np.complex_, one: np.complex_):
 
-        self.bra = bra
-        self.ket = ket
-        self.state = np.array([self.bra, self.ket], dtype=np.complex_)
+        self.zero = zero
+        self.one = one
+        self.state = np.array([self.zero, self.one], dtype=np.complex_)
         self._validate_state()
 
     def _validate_state(self):
-        if qubit_norm(self.state) != 1.0:
+        if not np.isclose(qubit_norm(self.state), 1.0):
             raise ValueError('state definition does not have unit norm')
 
-    def to_euler_angles(self):
-        global_phase = -np.angle(self.bra)
-        standard_bra = self.bra * np.sin(global_phase) + np.cos(global_phase) * 1j
-        standard_ket = self.ket * np.sin(global_phase) + np.cos(global_phase) * 1j
-        theta = 2 * np.arccos( standard_bra )
-        phi = np.angle(standard_ket / (2 * theta))
+    def to_euler(self):
+        # global phase rotation to achieve purely real |0>
+        global_phase = -np.angle(self.zero)
+        standard_zero = self.zero * (np.cos(global_phase) + np.sin(global_phase) * 1j)
+        standard_one = self.one * (np.cos(global_phase) + np.sin(global_phase) * 1j)
+        theta = 2 * np.arccos(standard_zero)
+        phi = 0   # default value for phi when state equals |0>
+        if theta != 0.0:  # i.e. state is not exactly |0>
+            phi = np.angle(standard_one)
         return QubitEuler(theta=theta, phi=phi)
 
 
@@ -36,13 +39,8 @@ class QubitEuler:
         self.state = np.array((np.cos(theta/2),
                                np.sin(theta/2)*(np.cos(phi)+np.sin(phi)*1j)),
                               dtype=np.complex_)
-        self._validate_state()
-
-    def _validate_state(self):
-        if qubit_norm(self.state) != 1.0:
-            raise ValueError('state definition does not have unit norm')
 
     def to_cartesian(self):
-        bra = self.state[0]
-        ket = self.state[1]
-        return QubitCartesian(bra=bra, ket=ket)
+        zero = self.state[0]
+        one = self.state[1]
+        return QubitCartesian(zero=zero, one=one)
--- test/test_qubit.py
+++ test/test_qubit.py
@@ -5,12 +5,37 @@ import numpy as np
 from qubit import QubitCartesian, QubitEuler
 
 
+class TestQubitCartesian(unittest.TestCase):
+
+    def test_to_cartesian(self):
+        q = QubitCartesian(zero=1, one=0)
+        q_e = q.to_euler()
+        self.assertAlmostEqual(q_e.theta, 0)
+        self.assertAlmostEqual(q_e.phi, 0)
+
+        q = QubitCartesian(zero=0, one=1j)
+        q_e = q.to_euler()
+        self.assertAlmostEqual(q_e.theta, np.pi)
+        self.assertAlmostEqual(q_e.phi, np.pi/2)
+
+        q = QubitCartesian(zero=np.sqrt(5)/5+2/5*1j, one=4/5)
+        q_e = q.to_euler()
+        self.assertAlmostEqual(q_e.theta, 2*np.arccos(0.6))
+        self.assertAlmostEqual(q_e.phi, -np.arctan(2/np.sqrt(5)))
+
+
 class TestQubitEuler(unittest.TestCase):
 
     def test_to_cartesian(self):
         q = QubitEuler(theta=0, phi=0)
         q_c = q.to_cartesian()
-        self.assertAlmostEquals(q_c.bra, 1+0j)
+        self.assertAlmostEqual(q_c.zero, 1)
+        self.assertAlmostEqual(q_c.one, 0)
+
+        q = QubitEuler(theta=np.pi, phi=np.pi/2)
+        q_c = q.to_cartesian()
+        self.assertAlmostEqual(q_c.zero, 0)
+        self.assertAlmostEqual(q_c.one, 1j)
 
 
 if __name__ == '__main__':
